ac804f4b45a8934b42f837f0bb9bb0447d0b2677
WALL_E322 <omidomidian6113@gmail.com>
2022-05-05 02:25:24 +0300

    add some comments

--- Q16/oracle/EthPriceOracle.sol
+++ Q16/oracle/EthPriceOracle.sol
@@ -1,22 +1,35 @@
 pragma solidity 0.5.0;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./CallerContractInterface.sol";
+
 contract EthPriceOracle is Ownable {
+
   uint private randNonce = 0;
   uint private modulus = 1000;
-  mapping(uint256=>bool) pendingRequests;
+
+  mapping(uint256=>bool) pendingRequests;//the initial value for bool is FALSE
+
   event GetLatestEthPriceEvent(address callerAddress, uint id);
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+
   function getLatestEthPrice() public returns (uint256) {
+
     randNonce++;
-    uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+    uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;//generate a 3-digit random number
+
     pendingRequests[id] = true;
     emit GetLatestEthPriceEvent(msg.sender, id);
     return id;
   }
+
   function setLatestEthPrice(uint256 _ethPrice, address _callerAddress,   uint256 _id) public onlyOwner {
+
     require(pendingRequests[_id], "This request is not in my pending list.");
     delete pendingRequests[_id];
-    // Start here
+
+    CallerContractInterface callerContractInstance;
+    callerContractInstance = CallerContractInterface(_callerAddress);
+    callerContractInstance.callback(_ethPrice, _id);
+    emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
   }
 }
