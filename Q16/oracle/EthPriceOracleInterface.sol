fd32db56fc63890098c40c6e6255bea50fc57c8b
WALL_E322 <omidomidian6113@gmail.com>
2022-05-05 02:31:33 +0300

    add some comments

--- Q16/oracle/EthPriceOracleInterface.sol
+++ Q16/oracle/EthPriceOracleInterface.sol
@@ -1,4 +1,8 @@
 pragma solidity 0.5.0;
+
+/*The interface contains function signatures without the function definition implementation.
+ You can use an interface in your contract to call functions in another contract.*/
+ 
 contract EthPriceOracleInterface {
   function getLatestEthPrice() public returns (uint256);
 }
